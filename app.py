fe5c123289a1f06909fc90cdfc1163c09b0ec587
SaiCharan123456 <77579506+SaiCharan123456@users.noreply.github.com>
2022-02-11 20:39:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+from classifier import  get_prediction
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/predict-alphabet", methods=["POST"])
+def predict_data():
+  # image = cv2.imdecode(np.fromstring(request.files.get("digit").read(), np.uint8), cv2.IMREAD_UNCHANGED)
+  image = request.files.get("alphabet")
+  prediction = get_prediction(image)
+  return jsonify({
+    "prediction": prediction
+  }), 200
+
+if __name__ == "__main__":
+  app.run(debug=True)
